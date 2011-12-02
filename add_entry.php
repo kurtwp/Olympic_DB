da413c2306291d1390568b0596cb9d6fafa840b1
Kurt Pasewaldt <kurt@pasewaldt.com>
2011-12-02 08:08:51 -0500

    removed all isset functions

--- add_entry.php
+++ add_entry.php
@@ -1,26 +1,32 @@
 <?php
-require_once 'login.php';
-require_once 'functions.php';
+header("Cache-Control: no-cache, must-revalidate");
 
-$db_server = mysql_connect($db_hostname, $db_username, $db_password);
-if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
-mysql_select_db($db_database)
-	or die("Unable to select database: " . mysql_error());
-$address1 = ""; // Address
-$f_name = ""; // First Name
-$l_name =""; // Last Name
-$error =""; // Error message
-$twork = $thome = $cell = $fax = "";
-if (isset($_POST['f_name']))
-{
-  $f_name = sanitizeString($_POST['f_name']);
-  $l_name = sanitizeString($_POST['l_name']);
-  
-  if ($f_name == "" || $l_name == "")
+require_once 'functions.php';
+include 'db_connect.php';
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+//****************************************
+// Check Customer Name Entry
+// f_name = First name
+// l_name = Last name
+//****************************************
+  if ($_POST['f_name'])
   {
-    $error = "Not all fields were entered<br />";
+    $f_name = sanitizeMySQL($_POST['f_name']);
   }
   else
+  {
+    echo "Please enter First Name";
+  }
+  if ($_POST['l_name'])
+  {
+    $l_name = sanitizeMySQL($_POST['l_name']);
+  }
+  else
+  {
+    echo "Please enter Last Name";
+  }
+  
+  if (!empty($f_name) && !empty($l_name))
   {
     $query = "INSERT INTO master_name VALUES(NULL, '$f_name', '$l_name')";
     $f_name = $l_name = "";
@@ -28,61 +34,106 @@ if (isset($_POST['f_name']))
     $masterID = mysql_insert_id();
     if (!$results) die ("Database access failed: " . mysql_error());
   }
-}
-
-if (isset($_POST['address1']))
-{
-  $address1 = sanitizeString($_POST['address1']);
-  $address2 = sanitizeString($_POST['address2']);
-  $city = sanitizeString($_POST['city']);
-  $state = sanitizeString($_POST['state']);
-  $zipcode = sanitizeString($_POST['zipcode']);
-  $add_type = sanitizeString($_POST['add_type']);
-
-  if ($address1 == "" && $city == "")
+//*************************************************************
+// Customer Address Entry check
+// $address1 = Street Address
+// $address2 = addition address information
+// $city = City name
+// $state = Two letter state
+// $zipcode = City zip code
+// $add_type = type of address and be only home, work, or other
+//*************************************************************
+  if ($_POST['address1'])
   {
-    $error = "Not all fields were entered in address table <br />";
+    $address1 = sanitizeMySQL($_POST['address1']);
   }
   else
+  {
+    echo "Please enter a street address";
+  }
+  if ($_POST['address2']) $address2 = sanitizeMySQL($_POST['address2']);
+  if ($_POST['city'])
+  {
+    $city = sanitizeMySQL($_POST['city']);
+  }
+  else
+  {
+    echo "Please enter a city";
+  }
+  if ($_POST['state'])
+  {
+    $state = sanitizeMySQL($_POST['state']);
+  }
+  else
+  {
+    echo "Please enter a state";
+  }
+  if ($_POST['zipcode'])
+  {
+    $zipcode = sanitizeMySQL($_POST['zipcode']);
+  }
+  else
+  {
+    echo "Please enter a zip code";
+  }
+  if ($_POST['add_type']) $add_type = sanitizeMySQL($_POST['add_type']);
+
+  if (!empty($address1) && !empty($city) && !empty($zipcode) )
   {
     $query = "INSERT INTO address VALUES(NULL,'$masterID','$address1','$address2','$city','$state','$zipcode','$add_type')";
     $results = mysql_query($query);
     if (!$results) die ("Database access failed: " . mysql_error());
   }
-}
-
-if (isset($_POST['sibling1']))
-{
-  $sibling1 = sanitizeString($_POST['sibling1']);
-  $sibling2 = sanitizeString($_POST['sibling2']);
-  $sibling3 = sanitizeString($_POST['sibling3']);
-  $subdivision = sanitizeString($_POST['subdivision']);
-
-  $query = "INSERT INTO sibling VALUES(NULL,'$masterID','$sibling1','$sibling2','$sibling3','$subdivision')";
-  $results = mysql_query($query);
-  if (!$results) die ("Database access failed: " . mysql_error());
-}
-// Enter data into EMAIL table
-if (isset($_POST['ework']) || isset($_POST['ehome']))
-{
- /* $ework = sanitizeString($_POST['ework']);
-  $ehome = sanitizeString($_POST['ehome']);
-  $query = "INSERT INTO email VALUES(NULL,'$masterID','$ehome','$ework')";
-    $results = mysql_query($query);
-    if (!$results) die ("Database access failed: " . mysql_error()); */
-}
-// Enter data into TELEPHONE table
-if (isset($_POST['twork']) || isset($_POST['thome']) || isset($_POST['cell']))
-{
-  /*$twork = sanitizeString($_POST['twork']);
-  $thome = sanitizeString($_POST['thome']);
-  $cell = sanitizeString($_POST['cell']);
-  $fax = sanitizeString($_POST['fax']);
-  $query = "INSERT INTO telephone VALUES(NULL,'$masterID','$thome','$twork','$cell','$fax')";
+//*********************************************************
+// Check Sibling data
+// $sibling1 = First child
+// $sibling2 = Second child
+// $sibling3 = Thrid child
+// $subdivision = The development the are picked up in
+//*********************************************************
+  if ($_POST['sibling1']) $sibling1 = sanitizeMySQL($_POST['sibling1']);
+  if ($_POST['sibling2']) $sibling2 = sanitizeMySQL($_POST['sibling2']);
+  if ($_POST['sibling3']) $sibling3 = sanitizeMySQL($_POST['sibling3']);
+  if ($_POST['subdivision']) $subdivision = sanitizeMySQL($_POST['subdivision']);
+  if (!empty($sibling1))
+  {
+    $query = "INSERT INTO sibling VALUES(NULL,'$masterID','$sibling1','$sibling2','$sibling3','$subdivision')";
     $results = mysql_query($query);
     if (!$results) die ("Database access failed: " . mysql_error());
-  $twork = $thome = $cell = $fax = "";*/
-}
+  }
+  else
+  {
+    echo "Need to add at least one sibling";
+  }
+//*****************************************************************
+// Enter data into EMAIL table
+// $ework = Work email
+// $ehome = Home email
+//*****************************************************************
+  if ($_POST['ework']) $ework = sanitizeMySQL($_POST['ework']);
+  if ($_POST['ehome']) $ehome = sanitizeMySQL($_POST['ehome']);
+  if (!empty($ework) || !empty($ehome))
+  {
+    $query = "INSERT INTO email VALUES(NULL,'$masterID','$ehome','$ework')";
+    $results = mysql_query($query);
+    if (!$results) die ("Database access failed: " . mysql_error()); 
+  }
+// Enter data into TELEPHONE table
+  if ($_POST['twork']) $twork = sanitizeMySQL($_POST['twork']);
+  if ($_POST['thome']) $thome = sanitizeMySQL($_POST['thome']);
+  if ($_POST['cell']) $cell = sanitizeMySQL($_POST['cell']);
+  if ($_POST['fax']) $fax = sanitizeMySQL($_POST['fax']);
+  if (!empty($twork) || !empty($thome) || !empty($cell))
+  {
+    $query = "INSERT INTO telephone VALUES(NULL,'$masterID','$thome','$twork','$cell','$fax')";
+    $results = mysql_query($query);
+    if (!$results) die ("Database access failed: " . mysql_error());
+  }
+  else
+  {
+    echo "need at least one Telephone number";
+  }
+} 
 echo <<<_END
 <form action="add_entry.php" method="post">$error
 <p><strong>Parent Name</strong></p>
@@ -123,6 +174,7 @@ Home<input type='text' size='30' maxlength=,25' name='ehome' value='$ehome' /><b
     <li><a href="add_entry.php">Add another Customer</a></li>
     <li><a href="delete_entry.php">Delete a Customer</a></li>
     <li><a href="search_entry.php">Search a Customer</a></li>
-</ul>    
+</ul>
+
 _END;
 ?>
