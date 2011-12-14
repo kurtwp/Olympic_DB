934b13e499b08dc1c1d40d9bc47254445d38a783
Kurt Pasewaldt <kurt@pasewaldt.com>
2011-12-14 16:53:24 -0500

    added non mysql sanitize function

--- functions.php
+++ functions.php
@@ -1,9 +1,18 @@
 <?php
-function sanitizeString($var)
+function sanitizeMySQL($var)
+// Will sanitize Text and MySQL injection
 {
 	$var = strip_tags($var);
 	$var = htmlentities($var);
 	$var = stripslashes($var);
 	return mysql_real_escape_string($var);
+}
+function sanitizeString($var)
+// Sanitize Text input only
+{
+	$var = strip_tags($var);
+	$var = htmlentities($var);
+	$var = stripslashes($var);
+	return $var;
 }
 ?>
