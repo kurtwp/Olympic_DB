cc70a692de5d71d226907e91ab0b32af900f1965
Kurt Pasewaldt <kurt@pasewaldt.com>
2012-03-01 16:53:32 -0500

    Added telephone edit portion

--- edit_entry.php
+++ edit_entry.php
@@ -27,6 +27,12 @@ $get_sibling = mysql_query("SELECT *
                            FROM sibling
                            WHERE sibling.master_id=$edit_id");
 $sib = mysql_fetch_array($get_sibling);
+// ****** Get Telephone Numbers *************
+$get_tele = mysql_query ("SELECT *
+                                FROM telephone
+                                WHERE telephone.master_id=$edit_id");
+$tele = mysql_fetch_array($get_tele);
+
 // ************ Start of Edit *************** 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 // ************ Start of Address edit *******
@@ -44,6 +50,42 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   } else {
     $result = mysql_query("INSERT INTO address (master_id, address1, address2, city, state, zipcode)
                           VALUES ('$edit_id', '$address[eaddress1]', '$address[eaddress2]', '$address[ecity]', '$address[estate]', '$address[zipcode]')");
+    if (!$result) die ("MySQL query access failed: " . mysql_error());
+  }
+// ************ Start of Sibling  *********************
+  if ($_POST['esibling1']) $sib['esibling1'] = sanitizeMySQL($_POST['esibling1']);
+  if ($_POST['esibling2']) $sib['esibling2'] = sanitizeMySQL($_POST['esibling2']);
+  if ($_POST['esibling3']) $sib['esibling3'] = sanitizeMySQL($_POST['esibling3']);
+  $table = "sibling";
+  if (!$_POST['esibling1'] && !$_POST['esibling2'] && !$_POST['esibling3']) {
+    $result = mysql_query("DELETE FROM sibling
+                          WHERE master_id=$edit_id");
+  } elseif (match_column($table, $edit_id)) {
+  $result = mysql_query("UPDATE sibling
+                          SET sibling1='$sib[esibling1]',sibling2='$sib[esibling2]',sibling3='$sib[esibling3]'
+                          WHERE master_id=$edit_id");
+      if (!$result) die ("Database access failed: " . mysql_error());
+  } else {
+    echo "IN INSERT <br />";
+    $result = mysql_query("INSERT INTO sibling (master_id, sibling1, sibling2, sibling3, subdivision)
+                            VALUES ('$edit_id','$sib[esibling1]','$sib[esibling2]','$sib[esibling3]')");
+      if (!$result) die ("MySQL query access failed: " . mysql_error());
+  }
+// ************ End of Sibling edit *********
+// ************ Start of Telephone edit *****
+if ($_POST['twork']) $tele['twork'] = sanitizeMySQL($_POST['twork']);
+  if ($_POST['thome']) $tele['thome'] = sanitizeMySQL($_POST['thome']);
+  if ($_POST['tcell']) $tele['tcell'] = sanitizeMySQL($_POST['tcell']);
+  if ($_POST['tfax']) $tele['tfax'] = sanitizeMySQL($_POST['tfax']);
+  $table = "telephone";
+  if (match_column($table, $edit_id)){
+    $result = mysql_query("UPDATE telephone
+                          SET home='$tele[thome]', work='$tele[twork]', cell='$tele[tcell]', fax='$tele[tfax]'
+                          WHERE master_id=$edit_id");
+    if (!$result) die ("Database access failed: " . mysql_error());
+  } else {
+    $result = mysql_query("INSERT INTO telephone (master_id, home, work, cell, fax)
+                          VALUES ('$edit_id', '$tele[thome]', '$tele[twork]', '$tele[tcell]', '$tele[tfax]')");
     if (!$result) die ("MySQL query access failed: " . mysql_error());
   }
 // ************ Start of EMail edit ********* 
@@ -69,25 +111,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
       if (!$result) die ("Database access failed: " . mysql_error());
   }
 // ************ End of EMail edit *********************
-// ************ Start of Sibling  *********************
-  if ($_POST['esibling1']) $sib['esibling1'] = sanitizeMySQL($_POST['esibling1']);
-  if ($_POST['esibling2']) $sib['esibling2'] = sanitizeMySQL($_POST['esibling2']);
-  if ($_POST['esibling3']) $sib['esibling3'] = sanitizeMySQL($_POST['esibling3']);
-  $table = "sibling";
-  if (!$_POST['esibling1'] && !$_POST['esibling2'] && !$_POST['esibling3']) {
-    $result = mysql_query("DELETE FROM sibling
-                          WHERE master_id=$edit_id");
-  } elseif (match_column($table, $edit_id)) {
-  $result = mysql_query("UPDATE sibling
-                          SET sibling1='$sib[esibling1]',sibling2='$sib[esibling2]',sibling3='$sib[esibling3]'
-                          WHERE master_id=$edit_id");
-      if (!$result) die ("Database access failed: " . mysql_error());
-  } else {
-    echo "IN INSERT <br />";
-    $result = mysql_query("INSERT INTO sibling (master_id, sibling1, sibling2, sibling3, subdivision)
-                            VALUES ('$edit_id','$sib[esibling1]','$sib[esibling2]','$sib[esibling3]')");
-      if (!$result) die ("MySQL query access failed: " . mysql_error());
-  }
+
   session_start();
         $_SESSION['id'] = $edit_id;
   header('Location: view_entry.php');
@@ -103,14 +127,20 @@ City/State/Zipcode <br />
 <input type='text' size='25' maxlength='30' name='ecity' value='$address[city]' />
 <input type='text' size='5' maxlength=,2' name='estate' value='$address[state]' />
 <input type='text' size='10' maxlength='10' name='ezipcode' value='$address[zipcode]' /><br />
-  <p><strong>Email</strong></p>
-  Home: <input type='text' size='30' maxlength='25' name='ehome' value='$email[home]' /><br />
-  Work: <input type='text' size='30' maxlength='25' name='ework' value='$email[work]' /><br />
-  <p><strong>Child Names</strong></p>
-  Child1: <input type='text' size="20" maxlength='30' name='esibling1' value='$sib[sibling1]' /><br />
-  Child2: <input type='text' size="20" maxlength='30' name='esibling2' value='$sib[sibling2]' /><br />
-  Child3: <input type='text' size="20" maxlength='30' name='esibling3' value='$sib[sibling3]' /><br />
-  <input type='submit' value = 'Submit'>
+<p><strong>Children</strong></p>
+Child1: <input type='text' size="20" maxlength='30' name='esibling1' value='$sib[sibling1]' /><br />
+Child2: <input type='text' size="20" maxlength='30' name='esibling2' value='$sib[sibling2]' /><br />
+Child3: <input type='text' size="20" maxlength='30' name='esibling3' value='$sib[sibling3]' /><br />
+<p><strong> Customer Telephone Numbers</strong> </p>
+Work<input type='text' size='30' maxlength='25' name='twork' value='$tele[work]' /><br />
+Home<input type='text' size='30' maxlength=,25' name='thome' value='$tele[home]' /><br />
+Cell<input type='text' size='30' maxlength='25' name='tcell' value='$tele[cell]' /><br />
+Fax<input type='text' size='30' maxlength='25' name='tfax' value='$tele[fax]' /><br />
+<p><strong>Email</strong></p>
+Home: <input type='text' size='30' maxlength='25' name='ehome' value='$email[home]' /><br />
+Work: <input type='text' size='30' maxlength='25' name='ework' value='$email[work]' /><br />
+<p></p>
+<input type='submit' value = 'Submit'>
 <br />
 </form>
 <ul>
