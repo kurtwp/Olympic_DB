515346d543009e09e698a3247552088ddb44a34d
Kurt Pasewaldt <kurt@pasewaldt.com>
2012-03-19 14:46:39 -0400

    Remove the code to view entire customer and instead forwarding the output to view_entry.php

--- search_entry.php
+++ search_entry.php
@@ -4,6 +4,7 @@ require_once 'db_connect.php';
 $search_name = "";
 $edit_id = 0;
 $home = "";
+$mode = "";
 // Search Customer by Last name
 if ($_SERVER['REQUEST_METHOD'] ==  "POST"){
   if ($_POST['search_name']){
@@ -32,64 +33,23 @@ if ($_SERVER['REQUEST_METHOD'] ==  "POST"){
   }
 }
 // Check to make sure MODE is set and not empty
-if (isset($_GET['mode']) && !empty($_GET['mode'])){
-  $id = $_GET['id'];
-// View customer information
-  if ($_GET['mode'] == "view"){
-      $get_name = mysql_query("SELECT * FROM master_name
-                              WHERE master_name.id=$id") or die ("Issue selecting rows - " . mysql_error());
-      $row = mysql_num_rows($get_name);
-      echo "Showing Record of : ";
-      while ($line = mysql_fetch_assoc($get_name)){
-        echo $line["f_name"] . " " . $line["L_name"] . "<br /> ";
-      }
-      $get_address = mysql_query("SELECT * FROM address WHERE address.master_id=$id");
-      $rows = mysql_num_rows($get_address);
-      if ($rows > 0){
-        while ($line = mysql_fetch_assoc($get_address)) {
-          echo $line["address1"] . " " . $line["address2"] . "<br /> ";
-          echo $line["city"] . " " . $line['state'] . ", " . $line['zipcode'] . "<br />";
-          echo "Address Type : " . $line['type'] . "<br />";
-        }
-      } 
-      $get_telephone = mysql_query("SELECT * FROM telephone WHERE telephone.master_id=$id");
-      $rows = mysql_num_rows($get_telephone);
-      if ($rows > 0) {
-        while ($line = mysql_fetch_assoc($get_telephone)){
-          echo "HOME : " . $line['home'] . "<br /> ";
-          echo "WORK : " . $line['work'] . "<br /> ";
-          echo "CELL : " . $line['cell'] . "<br /> ";
-          echo " FAX : " . $line['fax'] . "<br />";
-          } 
-      } 
-      $get_sibling = mysql_query("SELECT * FROM sibling WHERE sibling.master_id=$id");
-      $rows = mysql_num_rows($get_sibling);
-      if ($rows > 0){
-        while ($line = mysql_fetch_assoc($get_sibling)){
-          echo "Child 1: " . $line["sibling1"] . "<br /> ";
-          echo "Child 2: " . $line["sibling2"] . "<br /> ";
-          echo "Child 3: " . $line['sibling3'] . "<br /> ";
-        } 
-      } 
-      $get_email = mysql_query("SELECT * FROM email WHERE email.master_id=$id");
-      $rows = mysql_num_rows($get_email);
-      if ($rows > 0) {
-        while ($line = mysql_fetch_assoc($get_email)){
-          echo "HOME: " . $line["home"] . "<br /> ";
-          echo "WORK: " . $line["work"] . "<br /> ";
-        } 
-      }
-      echo "Do you want to update this record<br />";
-	  echo "<a href=" . $_SERVER['PHP_SELF'] . "?id=" . $id . "&mode=edit>UPDATE</a>";
-  } // End if view
-// Redirected to edit_entry.php to edit/update customer information
-  if ($_GET['mode'] == "edit") {
-        session_start();
-        $_SESSION['id'] = $id;
-        header('Location: edit_entry.php');
-        exit;
-      }// End if edit*/
-} // End isset if
+  if(isset($_GET['mode'])) {
+    $mode = $_GET['mode'];
+  } 
+  if ($mode == "view") {
+    $id = $_GET['id'];
+    session_start();
+    $_SESSION['id'] = $id;
+    header('Location: view_entry.php');
+    exit;
+  }
+  if ($mode == "edit") {
+    $id = $_GET['id'];
+    session_start();
+    $_SESSION['id'] = $id;
+    header('Location: edit_entry.php');
+    exit;
+  }// End if edit*/
 echo <<<_END
 <form action="search_entry.php" method="post">
 <p><strong>Search by Last name only</strong></p>
